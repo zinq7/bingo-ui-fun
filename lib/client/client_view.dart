5419fd4eb06d3e1aa11f197792d7d5f8e74951ef
zinq <benfarmilo@gmail.com>
2023-09-19 15:16:09 -0400

    add planning for a bit more in the project

--- lib/client/client_view.dart
+++ lib/client/client_view.dart
@@ -25,6 +25,21 @@ class ClientViewState extends State<ClientView> {
   int _points = 0;
   ScreenshotController bingoScreenshotter = ScreenshotController();
 
+  String getTitle() {
+    switch (widget.width) {
+      case 5:
+        return "Plus5";
+      case 6:
+        return "Plus5!";
+      case 7:
+        return "Eclipse";
+      case 8:
+        return "Eclipse8";
+      default:
+        return "BingoButAwesome".substring(0, widget.width);
+    }
+  }
+
   List<String> getBoard() {
     return List<String>.from(widget.allBingoTiles);
   }
@@ -112,7 +127,7 @@ class ClientViewState extends State<ClientView> {
                   Column(
                     children: [
                       Text(
-                        "Plus5",
+                        getTitle(),
                         textScaleFactor: 2,
                         // textAlign: TextAlign.center,
                         style: getBingoTitleStyle(context),
---
+++ lib/host/host_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HostView extends StatefulWidget {
+  const HostView({super.key});
+
+  @override
+  State createState() => HostViewState();
+}
+
+class HostViewState extends State<HostView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
